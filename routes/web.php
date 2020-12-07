b09371147e9ca107a0c1ce05d757f647bc6cb0b3
k.k <dodk7119@gmail.com>
2020-12-07 13:38:01 +0000

    routes middleware

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 // ↑
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('tasks', 'TasksController', ['only' => ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']]);
+});
